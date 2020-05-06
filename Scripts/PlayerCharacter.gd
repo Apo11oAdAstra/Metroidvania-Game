bf67d5fe617257cdd49af6c5732e390488c66ad7
Jerdan1980 <jeremytinana@gmail.com>
2020-05-06 19:51:44 -0400

    add jumping to marisa

--- Scripts/PlayerCharacter.gd
+++ Scripts/PlayerCharacter.gd
@@ -3,22 +3,24 @@ extends KinematicBody2D
 #character settings
 var gravity = Vector2(0, 9.8)
 var acceleration = Vector2(20, 0)
-var friction = 1.1
+var friction = 10
+var jump_force = 400
 var sprite
 
 #state variables
 var velocity = Vector2()
 var input = Vector2()
 var right = true
+var jump_counter
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	sprite = get_node("marisa")
+	jump_counter = 0
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	#process gravity
-	velocity += gravity
+func _physics_process(delta):
+	
 	
 	#grab inputs
 	input = Vector2(0,0)
@@ -27,6 +29,18 @@ func _process(delta):
 	
 	#process inputs
 	velocity.x += input.normalized().x * acceleration.x
+	
+	#handle jumps
+	if jump_counter > 100:
+		jump_counter = 0
+	if Input.is_action_just_pressed("ui_up") and jump_counter == 0:
+		velocity.y = -jump_force
+		jump_counter += 1
+	if jump_counter >= 1:
+		jump_counter += 1
+	#process gravity
+	velocity += gravity
+	
 	#process friction
 	if input.x == 0:
 		velocity.x /= friction
