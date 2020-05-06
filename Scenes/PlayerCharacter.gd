49a83702b198cb14b955e19e9e0666dab145a5ba
Apo11oAdAstra <59679742+Apo11oAdAstra@users.noreply.github.com>
2020-05-06 19:49:26 -0400

    Collision Complete

---
+++ Scenes/PlayerCharacter.gd
@@ -0,0 +1,44 @@
+extends KinematicBody2D
+
+#character settings
+var gravity = Vector2(0, 9.8)
+var acceleration = Vector2(20, 0)
+var friction = 1.1
+var sprite
+
+#state variables
+var velocity = Vector2()
+var input = Vector2()
+var right = true
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	sprite = get_node("marisa")
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	#process gravity
+	velocity += gravity
+	
+	#grab inputs
+	input = Vector2(0,0)
+	input.x = float(Input.is_action_pressed("ui_right")) - float(Input.is_action_pressed("ui_left"))
+	input.y = float(Input.is_action_pressed("ui_down")) - float(Input.is_action_pressed("ui_up"))
+	
+	#process inputs
+	velocity.x += input.normalized().x * acceleration.x
+	#process friction
+	if input.x == 0:
+		velocity.x /= friction
+	
+	#actually move
+	velocity = move_and_slide(velocity)
+	
+	#flips the character horizontally
+	if not input.x == 0:
+		if input.x > 0 and right == false:
+			sprite.set_scale(Vector2(1,1))
+			right = true
+		elif input.x < 0 and right == true:
+			sprite.set_scale(Vector2(-1, 1))
+			right = false
